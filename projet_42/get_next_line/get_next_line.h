8fb12a232c528a6c10898affdda8d6cad64f9671
Elias MESTOUR <elmestou@e1r13p2.42.fr>
2022-04-12 16:59:09 +0200

    get_next_line

--- projet_42/get_next_line/get_next_line.h
+++ projet_42/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: elmestou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 15:30:04 by elmestou          #+#    #+#             */
-/*   Updated: 2022/03/31 16:05:25 by elmestou         ###   ########.fr       */
+/*   Updated: 2022/04/12 16:57:13 by elmestou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+char	*get_next_line(int fd);
+char	*copystr(char *nws, char *s1, char *s2, unsigned int len);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
 char	*ft_strdup( char *s);
+size_t	ft_strlen(char *s);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
